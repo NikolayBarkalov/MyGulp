3ca71d37eecc843fbd395b60e45e0dfaea2d1c5a
NikolayBarkalov <77251682+NikolayBarkalov@users.noreply.github.com>
2022-01-18 12:40:50 +0300

    Tasks for working with

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 const {src, dest } = require("gulp");
 
+// Плагины
+const fileInclude = require("gulp-file-include");
+const htmlmin = require("gulp-htmlmin");
+const size = require("gulp-size");
+
 
 //Обработка HTML
 const html = () => {
   return src("./src/html/*.html")
-  .pipe(dest("./public"))
-};
+    .pipe(fileInclude())
+    .pipe(size({ title: "До сжатия" }))
+    .pipe(htmlmin({
+      collapseWhitespace: true // Убрать все лишние сим. разметки
+    }))
+    .pipe(size({ title: "После сжатия" }))
+    .pipe(dest("./public"));
+}
 
 //Задачи 
 exports.html = html;
