5aa192f6a081b99c3bf7572cd3f39eb4f15313fe
NikolayBarkalov <77251682+NikolayBarkalov@users.noreply.github.com>
2022-01-18 16:25:41 +0300

    task font

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const pug = require('./task/pug.js');
 const scss = require('./task/scss.js');
 const js = require('./task/js.js');
 const img = require('./task/img.js');
+const font = require('./task/font.js');
 
 
 // Сервер
@@ -31,6 +32,7 @@ const watcher = () => {
   watch(path.scss.watch, scss).on("all", browserSync.reload);
   watch(path.js.watch, js).on("all", browserSync.reload);
   watch(path.img.watch, img).on("all", browserSync.reload);
+  watch(path.font.watch, font).on("all", browserSync.reload);
 }
 
 // Задачи 
@@ -38,10 +40,11 @@ exports.pug = pug;
 exports.scss = scss;
 exports.js = js;
 exports.img = img;
+exports.font = font;
 
 // Сборка
 exports.dev = series(
   clear,
-  parallel(pug, scss, js, img),
+  parallel(pug, scss, js, img, font),
   parallel(watcher, server)
 );
