28a3ed763d7e9230b30a9bc11ed5f0dc4ca0e8e2
NikolayBarkalov <77251682+NikolayBarkalov@users.noreply.github.com>
2022-01-18 13:29:04 +0300

    add pugs

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const notify = require("gulp-notify");
 const fileInclude = require("gulp-file-include");
 const htmlmin = require("gulp-htmlmin");
 const size = require("gulp-size");
+const pugs = require("gulp-pug");
 
 
 
@@ -30,6 +31,26 @@ const html = () => {
     .pipe(browserSync.stream());
 }
 
+//Обработка PUG
+const pug = () => {
+  return src("./src/pug/*.pug")
+    .pipe(plumber({
+      errorHandler: notify.onError(error => ({
+        title: "Ошибка в PUG",
+        message: error.message
+      }))
+    }))
+    .pipe(pugs({
+      pretty: true, //Сжимать???
+      data: {
+        news: require('./data/news.json')
+      }
+    }))
+    .pipe(dest("./public"))
+    .pipe(browserSync.stream());
+}
+
+
 // Удаление директории
 const clear = () => {
   return del("./public");
@@ -46,17 +67,17 @@ const server = () => {
 
 // Наблюдение
 const watcher = () => {
-  watch("./src/html/**/*.html", html);
+  watch("./src/pug/**/*.pug", pug);
 }
 
 // Задачи 
-exports.html = html;
+exports.pug = pug;
 exports.watch = watcher;
 exports.clear = clear;
 
 // Сборка
 exports.dev = series(
   clear,
-  html,
+  pug,
   parallel(watcher, server)
 );
